3a9ace0bf7502e821930e030f278c0ffbb5e43e9
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-11 18:45:17 +0400

    fixing svg in skills section-7

--- src/components/Skill/Skills.jsx
+++ src/components/Skill/Skills.jsx
@@ -48,9 +48,9 @@ import  MaterialUiLogo  from "./Logo/MaterialUiLogo";
 import  MongoDbLogo  from "./Logo/MongoDbLogo";
 import  NextJsLogo  from "./Logo/NextJsLogo";
 import  NodeJsLogo  from "./Logo/NodeJsLogo";
-import  NtlifyLogo  from "./Logo/NtlifyLogo";
+import  NetlifyLogo  from "./Logo/NetlifyLogo";
 import  PhotoShopLogo  from "./Logo/PhotoShopLogo";
-import  PhythonLogo  from "./Logo/PhythonLogo";
+import  PythonLogo  from "./Logo/PythonLogo";
 import  ReactHookLogo  from "./Logo/ReactHookLogo";
 import  ReactJsLogo  from "./Logo/ReactJsLogo";
 import  ReactRouterLogo  from "./Logo/ReactRouterLogo";
@@ -66,7 +66,7 @@ export const Skills = () => {
     const currentYear = new Date().getFullYear();
     const logos = [{"component": <PhotoShopLogo/>}]
 
-    const skills = [
+    const skills1 = [
         {
             "category": "Front-End",
             "list": [
@@ -124,61 +124,61 @@ export const Skills = () => {
             ]
         }
     ];
-    const skills1 = [
+    const skills = [
         {
             "category": "Front-End",
             "list": [
-                {"logo": <BootstrapLogo/>, "title": "HTML", "startingYear": 2019},
-                [cssLogo, "CSS", 2019],
-                [javaScriptLogo, "JavaScript", 2019],
-                [es6Logo, "ES6", 2019],
-                [bootstrapLogo, "Bootstrap", 2019],
-                [materialUiLogo, "Material UI", 2020],
-                [reactJsLogo, "ReactJs", 2020],
-                [reactHookLogo, "React-Hook", 2020],
-                [reduxLogo, "Redux", 2020],
-                [reactRouterLogo, "React-Router", 2020],
-                [nextJsLogo, "NextJs", 2020],
-                [typeScriptLogo, "TypeScrip", 2020]
+                {"logo": <HtmlLogo/>, "title": "HTML", "startingYear": 2019},
+                {"logo": <CssLogo/>, "title": "CSS", "startingYear": 2019},
+                {"logo": <JavaScriptLogo/>, "title": "JavaScript", "startingYear": 2019},
+                {"logo": <Es6Logo/>, "title": "ES6", "startingYear": 2019},
+                {"logo": <BootstrapLogo/>, "title": "Bootstrap", "startingYear": 2019},
+                {"logo": <MaterialUiLogo/>, "title": "Material UI", "startingYear": 2020},
+                {"logo": <ReactJsLogo/>, "title": "ReactJs", "startingYear": 2020},
+                {"logo": <ReduxLogo/>, "title": "Redux", "startingYear": 2020},
+                {"logo": <ReactHookLogo/>, "title": "React-Hook", "startingYear": 2020},
+                {"logo": <ReactRouterLogo/>, "title": "React-Router", "startingYear": 2020},
+                {"logo": <NextJsLogo/>, "title": "NextJs", "startingYear": 2020},
+                {"logo": <TypeScriptLogo/>, "title": "TypeScrip", "startingYear": 2020}
             ]
         },
         {
             "category": "Back-End",
             "list": [
-                [javaScriptLogo, "JavaScript", 2019],
-                [nodeJsLogo, "NodeJs (CRUD)", 2020],
-                [expressJsLogo, "ExpressJs", 2020],
-                [mongoDbLogo, "MongoDB", 2020]
+                {"logo": <JavaScriptLogo/>, "title": "JavaScript", "startingYear": 2019},
+                {"logo": <NodeJsLogo/>, "title": "NodeJs", "startingYear": 2020},
+                {"logo": <ExpressLogo/>, "title": "ExpressJs", "startingYear": 2020},
+                {"logo": <MongoDbLogo/>, "title": "MongoDB", "startingYear": 2020}
             ]
         },
         {
             "category": "Tools",
             "list": [
-                [vsCodeLogo, "VS code", 2019],
-                [gitLogo, "Git", 2020],
-                [chromeLogo, "Chrome Dev tool", 2020],
-                [codeblocksLogo, "Code Blocks", 2017],
-                [devcppLogo, "Dev C++", 2017],
-                [figmaLogo, "Figma", 2020],
-                [photoshopLogo, "PhotoShop", 2019]
+                {"logo": <VsCodeLogo/>, "title": "VS code", "startingYear": 2020},
+                {"logo": <GitLogo/>, "title": "Git", "startingYear": 2020},
+                {"logo": <ChromeDevToolLogo/>, "title": "Chrome Dev tool", "startingYear": 2020},
+                {"logo": <CodeBlocksLogo/>, "title": "Code Blocks", "startingYear": 2017},
+                {"logo": <DevCppLogo/>, "title": "Dev C++", "startingYear": 2017},
+                {"logo": <FigmaLogo/>, "title": "Figma", "startingYear": 2020},
+                {"logo": <PhotoShopLogo/>, "title": "PhotoShop", "startingYear": 2019}
             ]
         },
         {
             "category": "Platform",
             "list": [
-                [githubLogo, "GitHub", 2020],
-                [firebaseLogo, "Firebase", 2020],
-                [netlifyLogo, "Netlify", 2020],
-                [vercelLogo, "Vercel", 2022]
+                {"logo": <GitHubLogo/>, "title": "GitHub", "startingYear": 2020},
+                {"logo": <FirebaseLogo/>, "title": "Firebase", "startingYear": 2020},
+                {"logo": <NetlifyLogo/>, "title": "Netlify", "startingYear": 2020},
+                {"logo": <VercelLogo/>, "title": "Vercel", "startingYear": 2022}
             ]
         },
         {
             "category": "Programming Language",
             "list": [
-                [cLogo, "C", 2017],
-                [cppLogo, "C++", 2018],
-                [javaScriptLogo, "JavaScript", 2019],
-                [pythonLogo, "Python", 2020]
+                {"logo": <CLogo/>, "title": "C", "startingYear": 2017},
+                {"logo": <CppLogo/>, "title": "C++", "startingYear": 2018},
+                {"logo": <JavaScriptLogo/>, "title": "JavaScript", "startingYear": 2019},
+                {"logo": <PythonLogo/>, "title": "Python", "startingYear": 2020}
             ]
         }
     ];
@@ -197,13 +197,13 @@ export const Skills = () => {
                                     <div className="skill">
                                         {d.list.map((s, i) => {
                                             return (
-                                                <div className="skillItem" key={`${s[1]}-${i}`}>
+                                                <div className="skillItem" key={`${s.logo}-${i}`}>
                                                     <div className="skillImg">
                                                         {/* <svg data-src={s[0]} width="30px" fill="#faebd7"></svg> */}
-                                                        {logos[0].component}
+                                                        {s.logo}
                                                     </div>
-                                                    <div className="skillName">{s[1]}</div>
-                                                    <div className="skillAge">{currentYear - s[2]}+ Years of<br />Experience </div>
+                                                    <div className="skillName">{s.title}</div>
+                                                    <div className="skillAge">{currentYear - s.startingYear}+ Years of<br />Experience </div>
                                                 </div>
                                             )
                                         }
