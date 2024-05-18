311d524017be5d6cb07391b818c31aaee3078707
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-18 11:19:11 +0400

    completing experience section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -15,9 +15,9 @@ const Experience = () => {
                 "location": "Abu Dhabi, United Arab Emirates.",
                 "duration": "December, 2023 - Present",
                 "description": [
-                    "Developed, maintained, and shipped production code for client websites using HTML, CSS, JavaScript, and ReactJS.",
-                    "Deliver high-quality, robust, cross browser compatibility and mobile responsiveness web applications for clients.",
-                    "Performed quality assurance tests on websites to ensure cross-browser compatibility and mobile responsiveness."
+                    "Create custom websites and web applications tailored to clients needs and specifications, utilizing a combination of HTML, CSS, JavaScript, and ReactJS to deliver modern, responsive, and user-friendly interfaces.",
+                    "Collaborate closely with clients to understand their vision, requirements, and feedback, offering expertise and guidance throughout the development process to ensure the final product meets or exceeds expectations.",
+                    "Manage projects independently, from initial concept and design to development, testing, and deployment, prioritizing tasks, and providing ongoing support to maintain and update websites as needed."
                 ]
             },
             {
@@ -37,8 +37,8 @@ const Experience = () => {
                 "duration": "June, 2022 - August, 2023",
                 "description": [
                     "Design, developed, maintained, and shipped interactive cross browser compatibility and mobile responsiveness web applications for the company using HTML, CSS, JavaScript, and ReactJS.",
-                    "Pioneered and built a website for the company",
-                    "Open and lead a new department (software department) in the company.",
+                    "Pioneered and built a website for the company.",
+                    "Built and lead a new department (software department) in the company.",
                 ]
             },
             {
@@ -47,9 +47,9 @@ const Experience = () => {
                 "location": "Dhaka, Bangladesh.",
                 "duration": "August 2021 - May 2022",
                 "description": [
-                    "Design, developed, maintained, and shipped interactive cross browser compatibility and mobile responsiveness web applications for the company using HTML, CSS, JavaScript, and ReactJS.",
-                    "Pioneered and built a website for the company",
-                    "Open and lead a new department (software department) in the company.",
+                    "Perform regular maintenance tasks such as software updates, system backups, and hardware diagnostics to ensure optimal performance and reliability of computer systems.",
+                    "Installing, configuring, and updating software and hardware, as well as troubleshooting and resolving technical issues",
+                    "Monitor network traffic and system logs to identify and troubleshoot potential security breaches or performance issues, implementing appropriate solutions to mitigate risks.",
                 ]
             },
             {
@@ -58,9 +58,9 @@ const Experience = () => {
                 "location": "Dhaka, Bangladesh.",
                 "duration": "February 2020 - July 2021",
                 "description": [
-                    "Design, developed, maintained, and shipped interactive cross browser compatibility and mobile responsiveness web applications for the company using HTML, CSS, JavaScript, and ReactJS.",
-                    "Pioneered and built a website for the company",
-                    "Open and lead a new department (software department) in the company.",
+                    "Design, deploy, and maintain network solutions tailored to meet the specific needs of clients, including LAN, WAN, and WLAN configurations, ensuring scalability, security, and performance.",
+                    "Conduct network assessments and audits to identify opportunities for optimization and improvement, recommending and implementing solutions to enhance network efficiency and reliability.",
+                    "Provide technical support and guidance to clients and internal teams, troubleshooting network issues, and assisting with the resolution of complex network-related challenges to ensure seamless operations.",
                 ]
             },
             {
@@ -69,9 +69,9 @@ const Experience = () => {
                 "location": "Dhaka, Bangladesh.",
                 "duration": "November 2019 - January 2020",
                 "description": [
-                    "Design, developed, maintained, and shipped interactive cross browser compatibility and mobile responsiveness web applications for the company using HTML, CSS, JavaScript, and ReactJS.",
-                    "Pioneered and built a website for the company",
-                    "Open and lead a new department (software department) in the company.",
+                    "Follow experienced network engineers to gain practical knowledge and hands-on experience in configuring, troubleshooting, and maintaining network infrastructure.",
+                    "Assist with basic network tasks such as cable management, device installation, and equipment inventory to support ongoing projects and operations.",
+                    "Participate in training sessions and workshops to develop skills in network design, routing, switching, and security, contributing to professional growth and readiness for future responsibilities in the field.",
                 ]
             }
         ]
