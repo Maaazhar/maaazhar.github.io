63ec46cdc379fad79aa8f9566f73db7d96dd5729
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-19 18:33:20 +0400

    adding project section

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,4 +1,5 @@
-import "./Project.css"
+import "./Project.css";
+import imgRafada from "../../assets/images/project/rafada.png";
 
 const Project = () => {
     return (
@@ -6,7 +7,94 @@ const Project = () => {
             <section className="project" id="project">
                 <div className="max-width">
                     <h2 className="title">Recent projects</h2>
-                    
+                    <div className="projectContentWrapper">
+                        <div className="projectContent">
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject">
+                                    <div className="projectImg"><img src="" alt="" /></div>
+                                    <div className="projectDescription">
+                                        <div className="projectTitle">
+                                            <h3>ProjectTitle</h3>
+                                        </div>
+                                        <div className="projectInfo">
+                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                        </div>
+                                        <div className="projectTechnology">
+                                            <ul>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject">
+                                    <div className="projectImg"><img src="" alt="" /></div>
+                                    <div className="projectDescription">
+                                        <div className="projectTitle">
+                                            <h3>ProjectTitle</h3>
+                                        </div>
+                                        <div className="projectInfo">
+                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                        </div>
+                                        <div className="projectTechnology">
+                                            <ul>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject">
+                                    <div className="projectImg"><img src="" alt="" /></div>
+                                    <div className="projectDescription">
+                                        <div className="projectTitle">
+                                            <h3>ProjectTitle</h3>
+                                        </div>
+                                        <div className="projectInfo">
+                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                        </div>
+                                        <div className="projectTechnology">
+                                            <ul>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject">
+                                    <div className="projectImg"><img src="" alt="" /></div>
+                                    <div className="projectDescription">
+                                        <div className="projectTitle">
+                                            <h3>ProjectTitle</h3>
+                                        </div>
+                                        <div className="projectInfo">
+                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                        </div>
+                                        <div className="projectTechnology">
+                                            <ul>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                                <li>HTML</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <h3>This section is under maintenance</h3>
                 </div>
             </section>
