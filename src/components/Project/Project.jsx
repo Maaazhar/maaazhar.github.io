6569655c873f9b5342c891f3c46547cd370aeab5
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-20 15:49:02 +0400

    updating project section-3

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -34,6 +34,106 @@ const Project = () => {
                                     </div>
                                 </div>
                             </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject ">
+                                    <div className="projectImg "><img src={imgOfRafada} alt="" /></div>
+                                    <div className="projectDescription ">
+                                        <div className="projectTitle">
+                                            <h3>Website of RAFADA Group</h3>
+                                        </div>
+                                        <div className="projectInfo ">
+                                            <p className="projectDetails">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                            <div className="projectTechnology">
+                                                <ul>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                </ul>
+                                            </div>
+                                            <div className="projectLink ">
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject ">
+                                    <div className="projectImg "><img src={imgOfRafada} alt="" /></div>
+                                    <div className="projectDescription ">
+                                        <div className="projectTitle">
+                                            <h3>Website of RAFADA Group</h3>
+                                        </div>
+                                        <div className="projectInfo ">
+                                            <p className="projectDetails">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                            <div className="projectTechnology">
+                                                <ul>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                </ul>
+                                            </div>
+                                            <div className="projectLink ">
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject ">
+                                    <div className="projectImg "><img src={imgOfRafada} alt="" /></div>
+                                    <div className="projectDescription ">
+                                        <div className="projectTitle">
+                                            <h3>Website of RAFADA Group</h3>
+                                        </div>
+                                        <div className="projectInfo ">
+                                            <p className="projectDetails">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                            <div className="projectTechnology">
+                                                <ul>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                </ul>
+                                            </div>
+                                            <div className="projectLink ">
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProject ">
+                                    <div className="projectImg "><img src={imgOfRafada} alt="" /></div>
+                                    <div className="projectDescription ">
+                                        <div className="projectTitle">
+                                            <h3>Website of RAFADA Group</h3>
+                                        </div>
+                                        <div className="projectInfo ">
+                                            <p className="projectDetails">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                            <div className="projectTechnology">
+                                                <ul>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                    <li>HTML</li>
+                                                </ul>
+                                            </div>
+                                            <div className="projectLink ">
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <h3>This section is under maintenance</h3>
