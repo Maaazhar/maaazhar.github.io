fcd8c62b693ace0e125d5b18335a012ea8068205
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-21 18:55:40 +0400

    updating project section-8

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -77,9 +77,40 @@ const Project = () => {
                                             <div className="projectTechnology">
                                                 <ul>
                                                     <li>HTML</li>
+                                                    <li>CSS</li>
+                                                    <li>JavaScript</li>
+                                                    <li>ReactJS</li>
+                                                    <li>NodeJS</li>
+                                                    <li>Bootstrap</li>
+                                                    <li>TailwindCSS</li>
+                                                </ul>
+                                            </div>
+                                            <div className="projectLink ">
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                                <a href="#"><i className="fab fa-github"></i></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="singleProjectWrapper">
+                                <div className="singleProjectRevers ">
+                                    <div className="projectImg "><img src={imgOfRafada} alt="" /></div>
+                                    <div className="projectDescription ">
+                                        <div className="projectTitle">
+                                            <h3>Website of Top Tier Technology</h3>
+                                        </div>
+                                        <div className="projectInfo ">
+                                            <p className="projectDetails">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
+                                            <div className="projectTechnology">
+                                                <ul>
                                                     <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
+                                                    <li>CSS</li>
+                                                    <li>JavaScript</li>
+                                                    <li>ReactJS</li>
+                                                    <li>NodeJS</li>
+                                                    <li>Bootstrap</li>
+                                                    <li>TailwindCSS</li>
                                                 </ul>
                                             </div>
                                             <div className="projectLink ">
@@ -102,9 +133,12 @@ const Project = () => {
                                             <div className="projectTechnology">
                                                 <ul>
                                                     <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
+                                                    <li>CSS</li>
+                                                    <li>JavaScript</li>
+                                                    <li>ReactJS</li>
+                                                    <li>NodeJS</li>
+                                                    <li>Bootstrap</li>
+                                                    <li>TailwindCSS</li>
                                                 </ul>
                                             </div>
                                             <div className="projectLink ">
@@ -116,20 +150,23 @@ const Project = () => {
                                 </div>
                             </div>
                             <div className="singleProjectWrapper">
-                                <div className="singleProject ">
+                                <div className="singleProjectRevers ">
                                     <div className="projectImg "><img src={imgOfRafada} alt="" /></div>
                                     <div className="projectDescription ">
                                         <div className="projectTitle">
-                                            <h3>Website of RAFADA Group</h3>
+                                            <h3>Website of Top Tier Technology</h3>
                                         </div>
                                         <div className="projectInfo ">
                                             <p className="projectDetails">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
                                             <div className="projectTechnology">
                                                 <ul>
                                                     <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
+                                                    <li>CSS</li>
+                                                    <li>JavaScript</li>
+                                                    <li>ReactJS</li>
+                                                    <li>NodeJS</li>
+                                                    <li>Bootstrap</li>
+                                                    <li>TailwindCSS</li>
                                                 </ul>
                                             </div>
                                             <div className="projectLink ">
@@ -142,7 +179,7 @@ const Project = () => {
                             </div>
                         </div>
                     </div>
-                    <h3>This section is under maintenance</h3>
+                    <h3 className="information">This section is under maintenance</h3>
                 </div>
             </section>
         </div>
