c2ba0eff8750cda32c1f7dd58919a673fdeadba4
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-21 18:40:32 +0400

    updating project section-6

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -21,13 +21,12 @@ const Project = () => {
                                             <div className="projectTechnology">
                                                 <ul>
                                                     <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
+                                                    <li>CSS</li>
+                                                    <li>JavaScript</li>
+                                                    <li>ReactJS</li>
+                                                    <li>NodeJS</li>
+                                                    <li>Bootstrap</li>
+                                                    <li>TailwindCSS</li>
                                                 </ul>
                                             </div>
                                             <div className="projectLink ">
@@ -39,20 +38,23 @@ const Project = () => {
                                 </div>
                             </div>
                             <div className="singleProjectWrapper">
-                                <div className="singleProject ">
+                                <div className="singleProjectRevers ">
                                     <div className="projectImg "><img src={imgOfRafada} alt="" /></div>
                                     <div className="projectDescription ">
                                         <div className="projectTitle">
-                                            <h3>Website of RAFADA Group</h3>
+                                            <h3>Website of Top Tier Technology</h3>
                                         </div>
                                         <div className="projectInfo ">
                                             <p className="projectDetails">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum ea ut tempore sint vitae natus beatae ad odit reprehenderit perspiciatis!</p>
                                             <div className="projectTechnology">
                                                 <ul>
                                                     <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
-                                                    <li>HTML</li>
+                                                    <li>CSS</li>
+                                                    <li>JavaScript</li>
+                                                    <li>ReactJS</li>
+                                                    <li>NodeJS</li>
+                                                    <li>Bootstrap</li>
+                                                    <li>TailwindCSS</li>
                                                 </ul>
                                             </div>
                                             <div className="projectLink ">
