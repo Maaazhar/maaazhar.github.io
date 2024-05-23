924aa21e249717e9c014ebf26a0c6a88bdb17a7f
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-23 14:48:38 +0400

    updating project section-14

--- src/components/Skill/Skills.jsx
+++ src/components/Skill/Skills.jsx
@@ -13,6 +13,25 @@ export const Skills = () => {
                     <div className="skillsContent">
                         {SkillList.map((d, i) => {
                             return (
+                                <>
+                                <div className="skillCategory" key={`${d.category}-${i}`}>
+                                    <label className="skillCategoryTitle" htmlFor={"skillAccordion-"+i}>{d.category}</label>
+                                    <input type="radio" name="SkillAccordion" id={"skillAccordion-"+i} />
+                                    <div className="skill">
+                                        {d.list.map((s, i) => {
+                                            return (
+                                                <div className="skillItem" key={`${s.logo}-${i}`}>
+                                                    <div className="skillImg">
+                                                        {s.logo}
+                                                    </div>
+                                                    <div className="skillName">{s.title}</div>
+                                                    <div className="skillAge">{currentYear - s.startingYear}+ Years of<br />Experience </div>
+                                                </div>
+                                            )
+                                        }
+                                        )}
+                                    </div>
+                                </div>
                                 <div className="skillCategory" key={`${d.category}-${i}`}>
                                     <div className="skillCategoryTitle">{d.category}</div>
                                     <div className="skill">
@@ -30,6 +49,7 @@ export const Skills = () => {
                                         )}
                                     </div>
                                 </div>
+                                </>
                             )
                         }
                         )}
