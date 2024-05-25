c5f3fa0fa6dc16500a0c64927f0c6f5955df96fa
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-25 13:56:18 +0400

    updating skill section-9

--- src/components/Skill/Skills.jsx
+++ src/components/Skill/Skills.jsx
@@ -1,12 +1,12 @@
 import "./Skills.css"
-import SkillList  from "./SkillList.jsx";
+import SkillList from "./SkillList.jsx";
 import { useState } from "react";
 
 export const Skills = () => {
     const currentYear = new Date().getFullYear();
     const [skillCategory, setSkillCategory] = useState(0)
 
-    const toggleCategory = (i) =>{
+    const toggleCategory = (i) => {
         setSkillCategory(i);
     }
 
@@ -19,25 +19,27 @@ export const Skills = () => {
                         {SkillList.map((d, i) => {
                             return (
                                 <>
-                                <div className="skillCategory" key={`${d.category}-${i}`}>
-                                    <label className="skillCategoryTitle" htmlFor={"skillAccordion-"+i}>{d.category} <i style={skillCategory === i ? {transform: "rotate(180deg)"} : {}} className="fas fa-angle-down"></i></label>
-                                    <input type="radio" name="SkillAccordion" value={i} id={"skillAccordion-"+i} onClick={() =>toggleCategory(i)} checked={skillCategory === i ? true : false}/>
-                                    <div className={"skill skill-"+i}>
-                                        {d.list.map((s, i) => {
-                                            return (
-                                                <div className="skillItem" key={`${s.logo}-${i}`}>
-                                                    <div className="skillImg">
-                                                        {s.logo}
-                                                    </div>
-                                                    <div className="skillName">{s.title}</div>
-                                                    <div className="skillAge">{currentYear - s.startingYear}+ Years of<br />Experience </div>
-                                                </div>
-                                            )
-                                        }
-                                        )}
+                                    <div className="skillCategory" key={`${d.category}-${i}`}>
+                                        <label className="skillCategoryTitle" htmlFor={"skillAccordion-" + i}>{d.category} <i style={skillCategory === i ? { transform: "rotate(180deg)" } : {}} className="fas fa-angle-down"></i></label>
+                                        <input type="radio" name="SkillAccordion" value={i} id={"skillAccordion-" + i} onClick={() => toggleCategory(i)} checked={skillCategory === i ? true : false} />
+                                        <div className="skillWrapper">
+                                            <div className={"skill skill-"}>
+                                                {d.list.map((s, i) => {
+                                                    return (
+                                                        <div className="skillItem" key={`${s.logo}-${i}`}>
+                                                            <div className="skillImg">
+                                                                {s.logo}
+                                                            </div>
+                                                            <div className="skillName">{s.title}</div>
+                                                            <div className="skillAge">{currentYear - s.startingYear}+ Years of<br />Experience </div>
+                                                        </div>
+                                                    )
+                                                }
+                                                )}
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                {/* <div className="skillCategory" key={`${d.category}-${i}`}>
+                                    {/* <div className="skillCategory" key={`${d.category}-${i}`}>
                                     <div className="skillCategoryTitle">{d.category}</div>
                                     <div className="skill">
                                         {d.list.map((s, i) => {
