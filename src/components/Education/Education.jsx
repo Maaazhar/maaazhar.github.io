da8026c8bd27a078d4e530c062c2b52cf259b91a
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-28 15:46:28 +0400

    updating project section-27

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import "./Education.css"
+
 const Education = () => {
+    const [educationCategory, setEducationCategory] = useState(0)
+    const [tabState, setTabState] = useState(0)
+    const toggleTab = (i) => setTabState(i);
+    const toggleCategory = (i) => setEducationCategory(i);
+
+    const educationList = [
+        {
+            "category": "",
+            "courseTitles": [],
+            "courseList": [
+                {
+                    "title": "",
+                    "institute": "",
+                    "universityBoard": "",
+                    "location": "",
+                    "passingYear": "",
+                    "majorSubject": [
+                        [],
+                        [],
+                        []
+                    ]
+                }
+            ]
+
+        }
+    ]
+
     return (
         <div>
             <section className="education" id="education">
                 <div className="max-width">
                     <h2 className="title">My Education</h2>
+
                     <h3  className="information">This section is under maintenance</h3>
                 </div>
             </section>
