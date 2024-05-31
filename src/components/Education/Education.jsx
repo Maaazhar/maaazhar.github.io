a7ea1a9362b0cebf7baa458edc6ae3c71273baf3
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-05-31 22:16:24 +0400

    updating education section-4

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -137,8 +137,12 @@ const Education = () => {
                                                     key={c - 1 + k}
                                                     className={tabState === k ? "educationTabContent activeEducationTabContent  " : "educationTabContent"}>
                                                     <h3 className="courseTitle">{c.title}</h3>
-                                                    <p className="institute"><a href={c.instituteLink} target="_blank">{c.institute}</a></p>
-                                                    <p className="universityBoard"><a href={c.universityBoardLink} target="_blank">{c.universityBoard}</a></p>
+                                                    <p className="institute">
+                                                        <a href={c.instituteLink} target="_blank">{c.institute}</a>
+                                                    </p>
+                                                    <p className="universityBoard">
+                                                        <a href={c.universityBoardLink} target="_blank">{c.universityBoard}</a>
+                                                    </p>
                                                     <p className="instituteLocation">{c.location}</p>
                                                     <p className="courseDuration">Course Duration: {c.duration}</p>
                                                     <p className="passingYear">Completed in the year of {c.passingYear}</p>
