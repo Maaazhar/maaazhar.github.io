3a8b787bad3a207f39a818da0622439c246fdbff
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-06-10 18:33:27 +0400

    updating database

---
+++ src/assets/database/SkillLogo/VercelLogo.jsx
@@ -0,0 +1,15 @@
+export const VercelLogo = () => {
+    return (
+        <div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="30px"
+                fill="#faebd7" >
+                <path d="m24 22.525h-24l12-21.05z" />
+            </svg>
+        </div>
+    )
+}
+
+export default VercelLogo
