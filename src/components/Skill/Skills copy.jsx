f9e90261145ee9754ae64825f00b34434d22a6c4
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-09-03 21:08:23 +0400

    updating skill section

---
+++ src/components/Skill/Skills copy.jsx
@@ -0,0 +1,56 @@
+import "./Skills.css"
+import SkillList from "./SkillList.jsx";
+import { useState } from "react";
+
+export const Skills = () => {
+    const currentYear = new Date().getFullYear();
+    const [skillCategory, setSkillCategory] = useState(0)
+    const toggleCategory = (i) => setSkillCategory(i);
+
+    return (
+        <section className="skills" id="skills">
+            <div className="max-width">
+                <h2 className="title">My skills</h2>
+                <div className="skillsContent">
+                    {SkillList.map((d, i) => (
+                        <div className="skillCategory accordion" key={d.category + i }>
+                            <input
+                                type="radio"
+                                id={"skillAccordion-" + i}
+                                className="accordionInput"
+                                name="SkillAccordion"
+                                value={i}
+                                onClick={() => toggleCategory(i)}
+                                defaultChecked={skillCategory === i ? true : false} />
+                            <label
+                                className="skillCategoryTitle accordionButton"
+                                htmlFor={"skillAccordion-" + i}>
+                                {d.category} <i className="fas fa-angle-down"> </i>
+                            </label>
+                            <div className="skillWrapper accordionContent">
+                                <div className="skill">
+                                    {d.list.map((s, i) => (
+                                        <div className="skillItem" key={s.logo + i} style={{ "--delay": i + 1 }}>
+                                            <div className="skillImg"> {s.logo} </div>
+                                            <div className="skillName"> {s.title} </div>
+                                            <div className="skillAge">
+                                                {currentYear - s.startingYear}+ Years of
+                                                <br />Experience
+                                            </div>
+                                        </div>
+                                    )
+                                    )}
+                                </div>
+                            </div>
+                            
+                        </div>
+                    )
+                    )}
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Skills;
+
