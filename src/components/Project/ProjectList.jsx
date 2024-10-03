8853088001626795b7f6ce60d78eaeeff9f694bf
Md. Mazharul Islam <mailformazhar@gmail.com>
2024-10-03 15:54:00 +0400

    updating the live link of msTube from vercel to github pages

--- src/components/Project/ProjectList.jsx
+++ src/components/Project/ProjectList.jsx
@@ -31,7 +31,7 @@ const projects = [
                 "image": msTube,
                 "title": "MS Tube a YouTube clone",
                 "details": "MS Tube is a YouTube clone app that streams videos using YouTube’s original links, offering a similar user experience for watching and exploring content while leveraging YouTube’s vast video library and functionality.",
-                "technology": ["ReactJS", "Material UI", "Vite", "Rapid API", "Axios", "Vercel", "Organic  SEO"],
+                "technology": ["ReactJS", "Material UI", "Vite", "Rapid API", "Axios", "GithubPages", "Organic  SEO"],
                 "liveLink": ["https://maaazhar.github.io/msTube/"],
                 "githubLink": "https://github.com/Maaazhar/msTube"
             },
